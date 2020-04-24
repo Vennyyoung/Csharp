808f4afa9c17fe7618212a4427fad179ff32da3b
Vennyyoung <57029756+Vennyyoung@users.noreply.github.com>
2020-04-24 10:35:49 +0100

    Update myGame.cs

--- myGame.cs
+++ myGame.cs
@@ -7,17 +7,14 @@ namespace myGame
         static void Main(string[] args)
         {
             string[] level = new string[3] { "EASY", "MEDIUM", "HARD" };
-            
             Random rnd = new Random ();
             int easy = rnd.Next (1, 11);
             int medium = rnd.Next (1, 21);
             int hard = rnd.Next (1, 51);
             int playerguess;
-            
-             Console.WriteLine ("[{0}]", string.Join (" ", level));
+            Console.WriteLine ("[{0}]", string.Join (" ", level));
             Console.Write ("Select Your Level: ");
             string player = Console.ReadLine();
-           
             for(int i = 1; i < 7; i++){
                 if(player == level[0]){
                     Console.Write("Pick a number from 1 - 10: ");
