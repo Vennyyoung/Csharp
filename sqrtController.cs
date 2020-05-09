52c2e07338703319a3f62eda96cf8f37ee86fe38
Vennyyoung <57029756+Vennyyoung@users.noreply.github.com>
2020-05-09 09:57:54 +0100

    Add files via upload

---
+++ sqrtController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+namespace sqrtChecker.Controllers
+{
+
+     public class sqrtController : Controller
+     {
+         public ActionResult Add()
+         {
+             return View();
+
+             
+
+        
+        
+         }
+
+
+     }
+
+}
