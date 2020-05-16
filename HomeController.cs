85e9ead017b23321a764ce52c088933c884506d6
Vennyyoung <57029756+Vennyyoung@users.noreply.github.com>
2020-05-16 23:25:48 +0100

    Update HomeController.cs

--- HomeController.cs
+++ HomeController.cs
@@ -18,19 +18,51 @@ namespace sqrtChecker.Controllers {
         public IActionResult Index () {
             return View ();
         }
-
-        public IActionResult Privacy () {
+[HttpPost]
+ public IActionResult SqrtChecker(string firstValue, string secondValue){
+     viewBag.Result = realSqrd(string firstValue, secondValue);
             return View ();
         }
 
-        public IActionResult SqrtChecker(){
-            return View ();
+     private static string realSqrd(string firstNum, string secondNum){
+         int firstValue;
+         int secondValue;
+         string present;
+         bool convertFirst = int.parse(firstNum, out firstNum);
+         bool convertSecond = int.parse(secondNum, out secondNum);
+         
+         
+         if(firstNum == "" || secondNum == ""){
+             present = "Please Enter a Number";
+     }
+     
+     else if (convertFirst && ConvertSecond){
+         if(firstValue < 0 || secondValue < 0){
+             present = "Math Error! Enter a positive value";}
+         else{
+             double Sqr1st = Math.Sqrt(firstValue);
+             double Sqr2nd = Math.Sqrt(secondValue);
+
+                    if (Sqr1st > Sqr2nd) {
+                        present = "The number " + firstValue + " with Square root " + Sqr1st + " has a Higher square root than the number " + secondValue + " with square root " + Sqr2nd";
+                    }
+                    else if (Sqr1st < Sqr2nd) {
+                        present = "The number " + secondvalue + " with Square root " + Sqr2nd + " has a Higher square root than the number " + firstvalue + " with square root " + Sqr1st;
+                    }
+                    else if (Sqr1st == Sqr2nd) {
+                        present = "Equal Values. Please enter different values";
+                    }
+                }
+            }
+            else {
+                present = "Invalid! Please enter a valid value";
+            }
+
+            return present;
         }
-
-        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error () {
-            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
